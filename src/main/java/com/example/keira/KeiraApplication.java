8f9fd92b6b85275920cc770e8ae1a1940c442d79
nightsun <dima.maximov53@gmail.com>
2022-12-05 19:55:13 +0300

    saves

---
+++ src/main/java/com/example/keira/KeiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.keira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keira/KeiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
